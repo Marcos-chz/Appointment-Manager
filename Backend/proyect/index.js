2e658b5222422061a594b29d8800995eab370fc0
Marcos <marcoschavezwebmail.com>
2026-01-22 15:40:11 -0300

    change backend to render

--- Backend/proyect/index.js
+++ Backend/proyect/index.js
@@ -5,15 +5,30 @@ const path = require('path')
 const multer = require("multer");
 
 const app = express();
-const port = 4000;
+const PORT = process.env.PORT || 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://appointment-manager.vercel.app" // tu frontend real
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
+
+
 app.use(express.json());
 
 app.use(
   "/uploads",
   express.static(path.join(__dirname, "uploads"))
 );
+
+
+app.get("/", (req, res) => {
+  res.status(200).json({ status: "API running" });
+});
 
 
 
@@ -40,8 +55,8 @@ app.use('/auth', authRouter);
 app.use('/availability', availabilityRouter)
 app.use('/pages', pagesController)
 
-app.listen(port, () => {
-  console.log('Server running at ' + port);
+app.listen(PORT, () => {
+  console.log('Server running at ' + PORT);
 });
 
 module.exports = upload;
